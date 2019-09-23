5344fcbd57d7a5a9aace86d2eda745e4bb293971
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2019-09-23 19:46:24 -0300

    config apidoc

--- src/app/controller/HomeController.js
+++ src/app/controller/HomeController.js
@@ -1,6 +1,20 @@
 class HomeController {
+    /**
+     * @api {get} / Home
+     * @apiGroup Home
+     *
+     * @apiSuccess {String} status Mensagem de acesso autorizado
+     *
+     * @apiSuccess {json} Sucesso
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "status": "OK!"
+     *    }
+     *
+     */
+
     index(request, response){
-        response.json({status: "Index"})
+        response.json({status: "Ok!"})
     }
 }
 
